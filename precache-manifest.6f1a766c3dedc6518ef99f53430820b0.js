6fef067c196578f831cc4335471bf8562a14854f
jeffry zaman <jeffry.zaman@gmail.com>
2019-07-08 11:34:47 -0400

    Updates

---
+++ precache-manifest.6f1a766c3dedc6518ef99f53430820b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d85cde6d6cca187c076739f833d330c",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "c64b72a903b7b14e8bb4",
+    "url": "/Weather/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0e84136b60fc7bbd5cca",
+    "url": "/Weather/static/css/main.108e67b2.chunk.css"
+  },
+  {
+    "revision": "c64b72a903b7b14e8bb4",
+    "url": "/Weather/static/js/2.830cce67.chunk.js"
+  },
+  {
+    "revision": "0e84136b60fc7bbd5cca",
+    "url": "/Weather/static/js/main.eb392019.chunk.js"
+  },
+  {
+    "revision": "e6172640d3267d8b322d",
+    "url": "/Weather/static/js/runtime~main.72be58e2.js"
+  },
+  {
+    "revision": "af79372058fec83861288a02dc004adb",
+    "url": "/Weather/static/media/cloud.af793720.png"
+  }
+]);
